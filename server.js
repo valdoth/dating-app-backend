be4c193046342dc47d105ad3305fb97fcd4d4162
valdoth <valdotsiarohasina@gmail.com>
2022-07-23 01:56:33 +0300

    make it better

---
+++ server.js
@@ -0,0 +1,48 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import Cards from './dbCards.js'
+import dotenv from 'dotenv'
+import Cors from 'cors'
+dotenv.config()
+
+// App config
+const app = express()
+const port = process.env.PORT || 8001
+
+const connection_url = process.env.CONNECTION_URL
+
+// Middleware
+app.use(express.json())
+app.use(Cors())
+
+// DB config
+mongoose.connect(connection_url, {
+
+})
+
+// API Endpoints
+app.get("/", (err, res, req) => res.status(200).send("Hello TheWebDev"))
+
+app.post('/dating/cards', (req, res) => {
+    const dbCard = req.body
+    Cards.create(dbCard, (err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(data)
+        }
+    })
+})
+app.get('/dating/cards', (req, res) => {
+    Cards.find((err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(data)
+        }
+    })
+})
+
+
+// Listener
+app.listen(port, () => console.log(`Listening on localhost: ${port}`))
