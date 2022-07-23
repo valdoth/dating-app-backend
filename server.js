75c967065d8babd29e52bfbbba0774af1d703a98
valdoth <valdotsiarohasina@gmail.com>
2022-07-23 04:22:40 +0300

    make it better

--- server.js
+++ server.js
@@ -14,14 +14,7 @@ const connection_url = process.env.CONNECTION_URL
 
 // Middleware
 app.use(express.json())
-// app.use(cors());
-app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
-    next();
-})
-
+app.use(cors());
 
 // DB config
 mongoose.connect(connection_url, {
